908a0f6167c5d63f1a97803184d739f1bf245dd3
arielysantos <arielys923@gmail.com>
2024-11-17 10:53:05 -0300

    mais alteraçoes do script GameManager

--- Jogo das Cores/Assets/GameManager.cs
+++ Jogo das Cores/Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public abstract class GameManagerBase : MonoBehaviour
@@ -7,44 +8,56 @@ public abstract class GameManagerBase : MonoBehaviour
     #region Singleton
     public static GameManagerBase instance;
 
-    // Método de inicialização do Singleton
-    protected virtual void Awake()
+    public int[] sequencia;
+    private object nomes;
+    private int acertos;
+    private int erros;
+    private readonly int corDaVez;
+
+    private void Start()
     {
-        if (instance == null)
+        // Chama o método da classe base
+        GerarSequencia();
+    }
+
+    // Sobrescreve o método GerarSequencia da classe base, se necessário
+    public override void GerarSequencia()
+    {
+        base.GerarSequencia();  // Chama a implementação base
+
+        // Ajuste da lógica de geração da sequência
+        sequencia = new int[Random.Range(3, Mathf.Max(nomes.Length, 3))];
+
+        for (int i = 0; i < sequencia.Length; i++)
         {
-            instance = this;
-            DontDestroyOnLoad(gameObject); // Opcional: mantém a instância entre as cenas
+            sequencia[i] = Random.Range(0, nomes.Length);
+            UIManager.instance.AtualizarSequencia(nomes[sequencia[i]]);
+        }
+    }
+
+    public override int GetCorDaVez()
+    {
+        return corDaVez;
+    }
+
+    // Implementa o comportamento específico para checar a cor
+    public override void ChecarCor(int corIndex, int corDaVez)
+    {
+        if (corIndex == sequencia[corDaVez])
+        {
+            corDaVez++;
+            if (corDaVez == sequencia.Length)
+            {
+                acertos++;
+                UIManager.instance.AtualizarAcertos(acertos);
+                GerarSequencia(); // Nova sequência
+            }
         }
         else
         {
-            Destroy(gameObject); // Destrói instâncias duplicadas
+            erros++;
+            UIManager.instance.AtualizarErros(erros);
+            GerarSequencia(); // Resetando a sequência
         }
     }
-    #endregion
-
-    // Atributos comuns a todos os gerenciadores
-    protected int corDaVez, acertos, erros;
-    protected string[] nomes;
-
-    public virtual void GerarSequencia()
-    {
-        corDaVez = 0;
-        UIManager.instance.LimparTexto();
-    }
-
-    // Métodos comuns para manipulação de acertos e erros
-    public void AtualizarAcertos(int acertos)
-    {
-        this.acertos = acertos;
-        UIManager.instance.AtualizarAcertos(acertos);
-    }
-
-    public void AtualizarErros(int erros)
-    {
-        this.erros = erros;
-        UIManager.instance.AtualizarErros(erros);
-    }
-
-    // Método que pode ser sobrescrito nas classes derivadas
-    public abstract void ChecarCor(int corIndex);
 }
