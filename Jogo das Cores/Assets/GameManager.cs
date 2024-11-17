3ede40de73ed5679e47d1b97bedfd2c23b6d7b7a
arielysantos <arielys923@gmail.com>
2024-11-17 11:09:35 -0300

    modificação do script GameManager

--- Jogo das Cores/Assets/GameManager.cs
+++ Jogo das Cores/Assets/GameManager.cs
@@ -8,6 +8,7 @@ public abstract class GameManagerBase : MonoBehaviour
     #region Singleton
     public static GameManagerBase instance;
 
+<<<<<<< Updated upstream
     public int[] sequencia;
     private object nomes;
     private int acertos;
@@ -58,6 +59,46 @@ public abstract class GameManagerBase : MonoBehaviour
             erros++;
             UIManager.instance.AtualizarErros(erros);
             GerarSequencia(); // Resetando a sequência
+=======
+    // Método de inicialização do Singleton
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Opcional: mantém a instância entre as cenas
+        }
+        else
+        {
+            Destroy(gameObject); // Destrói instâncias duplicadas
+>>>>>>> Stashed changes
         }
     }
+    #endregion
+
+    // Atributos comuns a todos os gerenciadores
+    protected int corDaVez, acertos, erros;
+    protected string[] nomes;
+
+    public virtual void GerarSequencia()
+    {
+        corDaVez = 0;
+        UIManager.instance.LimparTexto();
+    }
+
+    // Métodos comuns para manipulação de acertos e erros
+    public void AtualizarAcertos(int acertos)
+    {
+        this.acertos = acertos;
+        UIManager.instance.AtualizarAcertos(acertos);
+    }
+
+    public void AtualizarErros(int erros)
+    {
+        this.erros = erros;
+        UIManager.instance.AtualizarErros(erros);
+    }
+
+    // Método que pode ser sobrescrito nas classes derivadas
+    public abstract void ChecarCor(int corIndex);
 }
